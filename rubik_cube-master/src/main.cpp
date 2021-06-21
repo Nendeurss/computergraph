05e925b0f5daabd82b5b9b1ef6db57b1e21604f3
nendeurss <trankevintr@gmail.com>
2021-06-21 21:38:31 +0200

    empty view file

--- rubik_cube-master/src/main.cpp
+++ rubik_cube-master/src/main.cpp
@@ -4,7 +4,6 @@
 #include <GL/glut.h>
 #include <vector>
 #include "controller.cpp"
-#include "view.cpp"
 
 using namespace std;
 
@@ -65,7 +64,77 @@ void init_func ()
 
 
 
+void draw_cube(int x, int y, int z)
+{
 
+  vector<cube_rotate> lrot = cube_rotations[x][y][z];
+
+  glPushMatrix();
+
+  // translate to final position
+  glTranslatef((x - 1) * cube_size + x * gap, (y - 1) * cube_size + y * gap, (z - 1) * cube_size + z * gap);
+
+  // rotate cube to correct position
+  for(int i = lrot.size() - 1; i >= 0; --i)
+    glRotatef(lrot[i].angle, lrot[i].x, lrot[i].y, lrot[i].z);
+
+  glColor3f(1.0f, 0.0f, 0.0f);
+  glBegin(GL_QUADS);  // front
+    glNormal3f(0.0, 0.0, 1.0);  // face normal
+    glVertex3f(cube_size/2, cube_size/2, cube_size/2);
+    glVertex3f(-cube_size/2, cube_size/2, cube_size/2);
+    glVertex3f(-cube_size/2, -cube_size/2, cube_size/2);
+    glVertex3f(cube_size/2, -cube_size/2, cube_size/2);
+  glEnd();
+
+  glColor3f(1.0f, 0.5f, 0.0f);
+  glBegin(GL_QUADS);  // back
+    glNormal3f(0.0, 0.0, -1.0);  // face normal
+    glVertex3f(cube_size/2, cube_size/2, -cube_size/2);
+    glVertex3f(cube_size/2, -cube_size/2, -cube_size/2);
+    glVertex3f(-cube_size/2, -cube_size/2, -cube_size/2);
+    glVertex3f(-cube_size/2, cube_size/2, -cube_size/2);
+  glEnd();
+
+  glColor3f(0.0f, 0.0f, 1.0f);
+  glBegin(GL_QUADS);  // left
+    glNormal3f(-1.0, 0.0, 0.0);  // face normal
+    glVertex3f(-cube_size/2, cube_size/2, cube_size/2);
+    glVertex3f(-cube_size/2, cube_size/2, -cube_size/2);
+    glVertex3f(-cube_size/2, -cube_size/2, -cube_size/2);
+    glVertex3f(-cube_size/2, -cube_size/2, cube_size/2);
+  glEnd();
+  
+  glColor3f(0.0f, 1.0f, 0.0f);
+  glBegin(GL_QUADS);  // right
+    glNormal3f(1.0, 0.0, 0.0);  // face normal
+    glVertex3f(cube_size/2, cube_size/2, cube_size/2);
+    glVertex3f(cube_size/2, -cube_size/2, cube_size/2);
+    glVertex3f(cube_size/2, -cube_size/2, -cube_size/2);
+    glVertex3f(cube_size/2, cube_size/2, -cube_size/2);
+  glEnd();
+
+  glColor3f(1.0f, 1.0f, 1.0f);
+  glBegin(GL_QUADS);  // top
+    glNormal3f(0.0, 1.0, 0.0);  // face normal
+    glVertex3f(-cube_size/2, cube_size/2, -cube_size/2);
+    glVertex3f(-cube_size/2, cube_size/2, cube_size/2);
+    glVertex3f(cube_size/2, cube_size/2, cube_size/2);
+    glVertex3f(cube_size/2, cube_size/2, -cube_size/2);
+  glEnd();
+
+  glColor3f(1.0f, 1.0f, 0.0f);
+  glBegin(GL_QUADS);  // bottom
+    glNormal3f(0.0, -1.0, 0.0);  // face normal
+    glVertex3f(-cube_size/2, -cube_size/2, -cube_size/2);
+    glVertex3f(cube_size/2, -cube_size/2, -cube_size/2);
+    glVertex3f(cube_size/2, -cube_size/2, cube_size/2);
+    glVertex3f(-cube_size/2, -cube_size/2, cube_size/2);
+  glEnd();
+
+  glPopMatrix();
+
+} // draw cube function
             
 // draw function
 void draw_func(void)
@@ -90,7 +159,7 @@ void draw_func(void)
       for(int k = 0; k < 3; ++k) { // step through z axis
 
 	// draw a single cube
-	draw_cube(cube_rotations,i, j, k);
+	draw_cube(i, j, k);
 
       }
 
